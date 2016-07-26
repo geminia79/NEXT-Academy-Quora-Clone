ac3e60e16a9ec1009ec8fd0a5b678090757ee02e
Yung Chun Ern Martin <martinyce@gmial.com>
2016-07-26 14:46:34 +0800

    done with answers

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,27 @@
+require 'byebug'
+post '/answer' do
+	if logged_in?
+		answer = Answer.answer(params["answer"]["question_id"], session[:id], params["answer"]["answer"])
+		redirect to '/question/' + answer.question_id.to_s
+	else
+		@message = "Please log in first"
+		erb :'sessions/new'
+	end
+end
+
+get '/answer/:id/edit' do # return form to edit answer
+	@answer = Answer.find(params[:id])
+	erb :'answers/edit'
+end
+
+put '/answer/:id' do # update answer
+	answer = Answer.find(params[:id])
+	answer.update(params[:answer])
+	redirect to '/question/' + answer.question_id.to_s
+end
+
+delete '/answer/:id' do
+	answer = Answer.find(params[:id])
+	Answer.find(params[:id]).destroy
+	redirect to '/question/' + answer.question_id.to_s
+end
--- app/models/Answer.rb
+++ app/models/Answer.rb
@@ -1,6 +1,10 @@
 class Answer < ActiveRecord::Base
 	belongs_to :question
 	belongs_to :user
+
+	def self.answer(question_id, user_id, answer)
+		create(question_id: question_id, user_id: user_id, answer: answer)
+	end
 end
 
 
